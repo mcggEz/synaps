6bb6adba89acc5c3052e701144e9c6bdb8eeef13
mcggEz <112741425+mcggEz@users.noreply.github.com>
2025-05-06 22:26:34 +0800

    Updated some for deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable ESLint rules here
+      '@typescript-eslint/no-explicit-any': 'off', // Disable the 'no-explicit-any' rule
+      '@typescript-eslint/no-unused-vars': 'off', // Disable the 'no-unused-vars' rule
+      'next/next/no-img-element': 'off', // Disable the 'no-img-element' rule
+      'react-hooks/exhaustive-deps': 'off', // Disable the 'react-hooks/exhaustive-deps' rule
+    },
+  },
 ];
 
 export default eslintConfig;
